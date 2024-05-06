f6f7d7a21ce5a3f45e730b74ce8ffcf50aeb8b4c
yindaheng98 <yindaheng98@gmail.com>
2024-05-06 03:40:28 +0000

    configurable HashEncoder

--- contrib/mipnerf/python/jnerf/models/position_encoders/hash_encoder/hash_encoder.py
+++ contrib/mipnerf/python/jnerf/models/position_encoders/hash_encoder/hash_encoder.py
@@ -14,8 +14,15 @@ class HashEncoder(nn.Module):
         self.hash_func_header = f"""
 #define get_index(p0,p1,p2) {self.hash_func}
         """
-        self.encoder = GridEncode(self.hash_func_header, aabb_scale=aabb_scale, n_pos_dims=3, n_features_per_level=2,
-                                  n_levels=16, base_resolution=16, log2_hashmap_size=19, using_fp16=using_fp16)
+        self.encoder = GridEncode(
+            self.hash_func_header,
+            aabb_scale=aabb_scale,
+            n_pos_dims=n_pos_dims,
+            n_features_per_level=n_features_per_level,
+            n_levels=n_levels,
+            base_resolution=base_resolution,
+            log2_hashmap_size=log2_hashmap_size,
+            using_fp16=using_fp16)
         self.grad_type = 'float32'
         if using_fp16:
             self.grad_type = 'float16'
